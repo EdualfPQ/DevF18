334cb02c71dc0e0ad6837a41001dd408ff6c500d
Edualf Pacheco Quintero <edualf.quintero@coppel.com>
2022-10-10 15:16:56 -0600

    Commit de la pokedex

---
+++ Pokedex/PokemonDetails/PokemonDetailsServ.js
@@ -0,0 +1,13 @@
+class Service{
+    async consultarPokemon(id){
+        let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        let response = await axios.get(url)
+            // .then((result) => {
+            //     return result.data.results;
+            // })
+            // .catch((error) => {
+            //     return error;
+            // });
+        return response;
+    }
+}
